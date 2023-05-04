5839b26cf89c03212d3336a92815a31083ea494e
ducnguyen96 <levinguyen.dl@gmail.com>
2023-05-04 14:14:27 +0700

    calendar-widget

--- awesome/.config/awesome/components/pastel/top-panel.lua
+++ awesome/.config/awesome/components/pastel/top-panel.lua
@@ -18,6 +18,8 @@ local dpi = beautiful.xresources.apply_dpi
 -- import widgets
 local tag_list = require("widgets.tag-list")
 local brightness_widget = require("awesome-wm-widgets.brightness-widget.brightness")
+local calendar_widget = require("awesome-wm-widgets.calendar-widget.calendar")
+local volume_widget = require('awesome-wm-widgets.volume-widget.volume')
 
 -- define module table
 local top_panel = {}
@@ -37,19 +39,37 @@ top_panel.create = function(s)
       width = s.geometry.width,
    })
 
+   -- Create a textclock widget
+   mytextclock = wibox.widget.textclock()
+   local cw = calendar_widget({
+      theme = 'nord',
+      previous_month_button = 1,
+      next_month_button = 3,
+   })
+
+   mytextclock:connect_signal("button::press",
+      function(_, _, _, button)
+         if button == 1 then cw.toggle() end
+      end)
+
    panel:setup {
       expand = "none",
       layout = wibox.layout.align.horizontal,
       tag_list.create(s),
-      require("widgets.calendar").create(s),
+      -- require("widgets.calendar").create(s),
+      mytextclock,
       {
          layout = wibox.layout.fixed.horizontal,
          wibox.layout.margin(wibox.widget.systray(), dpi(5), dpi(5), dpi(5), dpi(5)),
-         require("widgets.bluetooth"),
-         require("widgets.network")(),
-         require("widgets.volume")(),
+         volume_widget {
+            widget_type = 'horizontal_bar',
+            with_icon = true,
+            mute_color = '#f77272',
+            refresh_rate = 10,
+         },
          brightness_widget(),
          require("widgets.battery"),
+         -- require("widgets.network")(),
          wibox.layout.margin(require("widgets.layout-box"), dpi(5), dpi(5), dpi(5), dpi(5))
       }
    }
